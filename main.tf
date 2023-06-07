51c0cbf592cbd9f242c29d7d944d5081144ab65c
ravitejachikkala <79794643+ravitejachikkala@users.noreply.github.com>
2023-06-07 19:35:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,18 +52,9 @@ module "eks_cluster" {
 resource "aws_subnet" "my_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
+  availability_zone       = "ap-south-1a, ap-south-1b"
   
   tags = {
     Name = "github-actions-sb1"
   }
 }
-resource "aws_subnet" "my_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1b"
-  
-  tags = {
-    Name = "github-actions-sb2"
-  }
-}  
