8b36643e016249f8870404a70e8facbc9b0493bc
ravitejachikkala <79794643+ravitejachikkala@users.noreply.github.com>
2023-06-07 19:32:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,3 +58,12 @@ resource "aws_subnet" "my_subnet" {
     Name = "github-actions-sb1"
   }
 }
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1b"
+  
+  tags = {
+    Name = "github-actions-sb2"
+  }
+}  
